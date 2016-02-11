9446d9506072e938131f48e15dc03a5916af01e9
Gabriel Rodríguez Fernández <gabrielrf96@gmail.com>
2016-02-11 17:17:45 +0000

    Eliminado elemento de depuración

--- js/stickynav.js
+++ js/stickynav.js
@@ -2,7 +2,6 @@ window.onscroll = function() {
 	var nav = document.getElementsByTagName("nav")[0];
 	var navOffset = document.getElementsByTagName("header")[0].offsetHeight;
 	var scrollBarPosition = window.pageYOffset || document.body.scrollTop;
-	document.getElementById("a").innerHTML = scrollBarPosition;
 	if (scrollBarPosition >= navOffset) {
 		nav.classList.add("fixedNav");
 	}
